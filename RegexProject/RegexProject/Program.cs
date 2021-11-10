4412dd217db9ab67129cddd8f20d884a07d85122
vish <vishgirase95@gmail.com>
2021-11-10 12:50:03 +0530

    [vishal] added Phone number

--- RegexProject/RegexProject/Program.cs
+++ RegexProject/RegexProject/Program.cs
@@ -12,7 +12,11 @@ namespace RegexProject
             Console.WriteLine("First name validation : "+obj.FirstNameValidate("Vishal"));
 
             Console.WriteLine("Last name Validation : " +obj.LastNameValidate("Girase"));
+            
             Console.WriteLine("Email Validation : " + obj.EmailIdValidate("vish.girase95@gmail.in"));
+
+            Console.WriteLine("Phone Number Validation : " + obj.PhoneNumberValidate("911 8806001896"));
+
 
 
         }
